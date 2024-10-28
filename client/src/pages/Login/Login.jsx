6728d54b7dcac91f6661ec0b6ae166fb78ac21b6
Igor Stein dos Santos <igorstein2001@gmail.com>
2024-10-28 14:42:52 -0300

    Melhorias

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -6,8 +6,14 @@ import FilledInput from "@mui/material/FilledInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
 import Visibility from "@mui/icons-material/Visibility";
+import Button from "@mui/joy/Button";
+import Input from "@mui/joy/Input";
+import Stack from "@mui/joy/Stack";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { Link } from "react-router-dom";
+import zorbs from "../../../public/assets/ZORBS.png";
 
 export default function Login() {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -25,36 +31,62 @@ export default function Login() {
   return (
     <>
       <div className={styles.bc}>
-        <h1>Login</h1>
+        <img src={zorbs} alt="esquilo" width={400} />
         <div className={styles.texts}>
-          <TextField
-            label="E-mail"
-            id="filled-size-normal"
-            defaultValue="Normal"
-            variant="filled"
-          />
+          <h1 style={{ marginBottom: "2rem", marginTop: "2rem" }}>Login</h1>
+          <div className={styles.secondBox}>
+            <div>
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  const formData = new FormData(event.currentTarget);
+                  const formJson = Object.fromEntries(formData.entries());
+                  alert(JSON.stringify(formJson));
+                }}
+              >
+                <Stack spacing={1}>
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    E-mail:
+                  </InputLabel>
+                  <TextField required id="outlined-required" />
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Senha:
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    required
+                    type={showPassword ? "text" : "password"}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          onMouseUp={handleMouseUpPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                  <Button type="submit" style={{ marginTop: "2rem" }}>
+                    Login
+                  </Button>
+                </Stack>
+              </form>
 
-          <div>
-            <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
-              <InputLabel htmlFor="filled-adornment-password">Senha</InputLabel>
-              <FilledInput
-                id="filled-adornment-password"
-                type={showPassword ? "text" : "password"}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      onMouseUp={handleMouseUpPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "column",
+                  marginTop: "20px",
+                }}
+              >
+                <Link style={{ color: "black" }}>Esqueceu sua senha?</Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- server/conexao.js
+++ server/conexao.js
@@ -1,8 +1,13 @@
-const db = {
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "zorbs"
-}
+import mysql from 'mysql2/promise';
 
-export default db;
+const conexao = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'zorbs',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+export default conexao;
