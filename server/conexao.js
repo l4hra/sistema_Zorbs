1f3c6856fd643c188ea993e73c2043e612510be2
Luiz Felipe <felipexavierluizbizio@gmail.com>
2024-10-31 21:38:56 -0300

    Ajustes em lista de usuários

--- server/conexao.js
+++ server/conexao.js
@@ -1,8 +1,13 @@
-const db = {
+import mysql from 'mysql2/promise'
+
+const conexao = mysql.createPool ({
     host: "localhost",
     user: "root",
     password: "",
-    database: "db_zorbs"
-}
+    database: "db_zorbs",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit:0
+});
 
 export default db;
