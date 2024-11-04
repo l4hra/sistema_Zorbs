8b7c7a0f5338da97075ac083f3a00c457c4625be
Igor Stein dos Santos <igorstein2001@gmail.com>
2024-11-04 08:56:00 -0300

    Alterações antigas

--- server/src/models/UserModel.js
+++ server/src/models/UserModel.js
@@ -1,11 +1,10 @@
-import mysql from 'mysql2/promise';
-import db from '../../conexao.js';
+import conexao from '../../conexao.js';
 
 
 // Função para vizualizar usuários
 export async function getUsers(req, res) {
     console.log('UsersController getUsers');
-    const conexao = mysql.createPool(db);
+    
     try {
         const [rows] = await conexao.query('SELECT * FROM users');
         res.status(200).json(rows);
@@ -23,7 +22,7 @@ export async function addUser(users) {
     //     return [400, errors];
     // }
 
-    const conexao = mysql.createPool(db);
+    
     const sql = `INSERT INTO users (name, passaword, confirm_ps, 
     email, telefone, type_of_acess, status)
     VALUES (?,?,?,?,?,?,?)`;
@@ -49,7 +48,7 @@ export async function addUser(users) {
 
 export async function deleteUser(id) {
 
-    const conexao = mysql.createPool(db);
+    
     const sql = 'DELETE FROM users WHERE id = ?';
 
     try {
@@ -74,7 +73,7 @@ export async function updateUsers(id, users) {
     //     return [400, errors];
     // }
 
-    const conexao = mysql.createPool(db);
+    
     const sql = `UPDATE users SET 
     name = ?,
     passaword = ?,
