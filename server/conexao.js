22c092706a68d9c96f0dc4e072315f1077dfad8f
Igor Stein <igorstein2001@gmail.com>
2024-11-18 19:51:31 -0300

    merge luiz e Igor

--- server/conexao.js
+++ server/conexao.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const conexao = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '154869',
+    password: '',
     database: 'db_zorbs',
     waitForConnections: true,
     connectionLimit: 10,
